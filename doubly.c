1c4803c5e0a8bbc0530046c81cf489fdaffaa3a3
13urdock <uyeong7181@gmail.com>
2023-07-24 13:39:32 +0900

    printing in another way

--- doubly.c
+++ doubly.c
@@ -17,25 +17,32 @@ Node* addNewNode(int value){
 
 void printReversedList(Node* node){
 	for(Node *curr = node; curr !=NULL; curr = curr->prev)
-		printf("%d", curr->value);
+		printf("%d ", curr->value);
 	printf("\n");	
 }
 
-void insertValue(Node *root, int value){
-	Node* tmp = addNewNode(value);	
+void printList(Node *root){
+	for(Node *curr = root; curr != NULL; curr = curr->next)
+		printf("%d ", curr->value);
+	printf("\n");
+}
+
+void insertValue(Node *root, int value){	
 	Node* p = root;
 	while (p->next != NULL){
 		p = p->next;
 	}
- 	p->next = tmp;
-	tmp->prev = p;
+ 	p->next = addNewNode(value);
+	p->next->prev = p;
 }
 
 int main(void){
 	Node* root = addNewNode(0);
 	insertValue(root, 1);
 	insertValue(root, 2);
+
 	printReversedList(root->next->next);
+	printList(root);
 
 	return 0;	
 }
